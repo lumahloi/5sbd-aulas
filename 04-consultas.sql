5b3173bea1ac7e73b5eebe8a49b7f37546e3588b
Lumah <115045300+lumahloi@users.noreply.github.com>
2025-04-03 10:34:25 -0300

    Update 04-consultas.sql

--- 04-consultas.sql
+++ 04-consultas.sql
@@ -3,13 +3,13 @@ SELECT * FROM clientes;
 --2. Exibir apenas os nomes e as cidades dos clientes:
 SELECT nome, cidade FROM clientes;
 --3. Listar todas as contas registradas, exibindo o número da conta e o saldo:
-SELECT numero_conta, saldo FROM contas;
+SELECT numero_conta, saldo FROM conta;
 --4. Liste os nomes dos clientes da cidade de Macaé:
 SELECT nome FROM clientes WHERE cidade = 'Macaé';
 --5. Exiba o código e o nome de todos os clientes com código entre 5 e 15:
 SELECT codigo, nome FROM clientes WHERE codigo BETWEEN 5 AND 15;
 --6. Exiba os clientes que moram em Niterói, Volta Redonda ou Itaboraí:
-SELECT nome FROM clientes WHERE cidade IN ('Niterói', 'Volta Redonda', 'Itaboraí');
+SELECT nome FROM clientes WHERE cidade IN ('Niterói', 'Volta Redonda', 'Itaboraí'); 
 --7. Exiba os nomes dos clientes que começam com a letra "F":
 SELECT nome FROM clientes WHERE nome LIKE 'F%';
 --8. Exiba uma frase com a seguinte estrutura para todos os clientes:
